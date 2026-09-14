c2ca8da13262bd65a286faffd0b0fbc1788dc2f2
carreramejiaeduardo87-blip <carreramejiaeduardo87@gmail.com>
2026-09-14 17:56:21 -0500

    js created fails to validate forms

--- js/inicio_administradores.js
+++ js/inicio_administradores.js
@@ -1,15 +1,9 @@
 function validarInformacion() {
     let nombre_administrador= document.getElementById("nombre_administrador").value;
     let contraseña_administrador = document.getElementById("contraseña_administrador").value;
-    let cant_producto = document.getElementById("cant_producto").value;
-    let descripcion_producto = document.getElementById("descripcion_producto").value;
-    let marca_producto = document.getElementById("marca_producto").value;
-    let valor_uni = document.getElementById("valor_uni").value;
-    let categoria_producto = document.getElementById("categoria_producto").value;
-    let ubi_producto = document.getElementById("ubi_producto").value;
     
 
-    if (!nombre_administrador || !contraseña_administrador || !cant_producto || !descripcion_producto || !marca_producto || !valor_uni || !categoria_producto || !ubi_producto) {
+    if (!nombre_administrador || !contraseña_administrador) {
         Swal.fire({
             position: "top-end",
             icon: "error",
@@ -20,93 +14,31 @@ function validarInformacion() {
     }
     else {
         console.log(
-            `Informacion del Producto: \n
+            `Informacion del admin: \n
             ${nombre_administrador} \n
-            ${contraseña_administrador} \n
-            ${cant_producto} \n
-            ${descripcion_producto} \n
-            ${marca_producto} \n
-            ${valor_uni} \n
-            ${categoria_producto} \n
-            ${ubi_producto}`
+            ${contraseña_administrador}`
         );
     }
 
-    if (!/^\d+$/.test(cod_producto)) {
-        console.log("Valor debe contener números")
+    }
+    if (!/^[a-zA-Z]+$/.test(nombre_administrador)) {
+        console.log("nombre debe contener solo letras")
         Swal.fire({
-            title: "Valor debe contener números",
+            title: "nombre debe contener ",
             icon: "error"
         });
         return;
     }
-    if (!/^[a-zA-Z]+$/.test(contraseña_administrador)) {
-        console.log("Contraseña debe contener letras")
+    
+    if (!/^[a-zA-Z0-9]+$/.test(contraseña_administrador)) {
+        console.log("La contraseña debe contener solo letras y números");
         Swal.fire({
-            title: "Contraseña debe contener ",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^\d+$/.test(cant_producto)) {
-        console.log("Valor debe contener números")
-        Swal.fire({
-            title: "Valor debe contener números",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^[a-zA-Z]+$/.test(descripcion_producto)) {
-        console.log("Descripcion debe contener letras")
-        Swal.fire({
-            title: "Descripcion debe contener letras",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^[a-zA-Z]+$/.test(marca_producto)) {
-        console.log("Marca debe contener letras")
-        Swal.fire({
-            title: "Marca debe contener letras",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^\d+$/.test(valor_uni)) {
-        console.log("Valor Unitario debe contener números")
-        Swal.fire({
-            title: "Valor Unitario debe contener números",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^[a-zA-Z]+$/.test(categoria_producto)) {
-        console.log("Categoria debe contener letras")
-        Swal.fire({
-            title: "Categoria debe contener letras",
-            icon: "error"
-        });
-        return;
-    }
-    if (!/^[a-zA-Z]+$/.test(ubi_producto)) {
-        console.log("Ubicacion debe contener letras")
-        Swal.fire({
-            title: "Ubicacion debe contener letras",
-            icon: "error"
-        });
-        return;
-    }
-        return;
+             title: " debe contener letras y números",
+             icon: "error"
+    });
+    return; 
+}
+
     
 
-    {
-    Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Informacion Guardada Correctamente",
-            showConfirmButton: false,
-            timer: 1500
-        });
-    }
-}
 document.getElementById("guardar").onclick = validarInformacion;
---
+++ js/registro_admionistradores.js
@@ -0,0 +1,64 @@
+function validarInformacion() {
+    let nombre_administrador= document.getElementById("nombre_administrador").value;
+    let cedula_administrador= document.getElementById("cedula_administrador").value;
+    let contraseña_administrador = document.getElementById("contraseña_administrador").value;
+    let telefono_administrador = document.getElementById("telefono_administrador").value;
+
+    if (!nombre_administrador || !cedula_administrador || !contraseña_administrador || !telefono_administrador ) {
+        Swal.fire({
+            position: "top-end",
+            icon: "error",
+            title: "Campos Incompletos",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+    else {
+        console.log(
+            `Informacion del Producto: \n
+            ${nombre_administrador} \n
+            ${cedula_administrador} \n
+            ${contraseña_administrador} \n
+            ${telefono_administrador}`
+        );
+
+
+
+        if (!/^[a-zA-Z]+$/.test(nombre_administrador)) {
+            console.log("Nombre debe contener letras")
+            Swal.fire({
+                title: "Nombre debe contener letras",
+                icon: "error"
+            });
+            return;
+        }
+
+        if (!/^\d+$/.test(cedula_administrador)) {
+            console.log("Valor debe contener números")
+            Swal.fire({
+                title: "Valor debe contener números",
+                icon: "error"
+            });
+            return;
+        }
+        if (!/^[a-zA-Z0-9_]{3,16}$/.test(contraseña_administrador)) {
+            console.log("contraseña administrador debe contener letras, números y guiones bajos")
+            Swal.fire({
+                title: "contraseña administrador debe contener letras, números y guiones bajos",
+                icon: "error"
+            });
+            return;
+        }
+        
+        if (!/^\d+$/.test(telefono_administrador)) {
+            console.log("telefono debe contener solo números")
+            Swal.fire({
+                title: "telefono debe contener solo números",
+                icon: "error"
+            });
+            return;
+        }
+        
+}
+}
+document.getElementById("guardar").onclick = validarInformacion;
